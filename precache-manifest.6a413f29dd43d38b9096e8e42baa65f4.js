49622c924835e292b91b7d17f0ad537aedde6d3c
Albin Poignot <albinpoignot@users.noreply.github.com>
2020-05-28 16:47:39 -0400

    Updates

---
+++ precache-manifest.6a413f29dd43d38b9096e8e42baa65f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03083b6cb9b0a958d97367aa7b9ad0c",
+    "url": "/dataviz/index.html"
+  },
+  {
+    "revision": "8c33e00a6856bcdfb2c1",
+    "url": "/dataviz/static/css/main.23011c0d.chunk.css"
+  },
+  {
+    "revision": "470c85d1a30f5a4285e9",
+    "url": "/dataviz/static/js/2.76a5d545.chunk.js"
+  },
+  {
+    "revision": "40cdae6d833b40535ce8a177d9d53093",
+    "url": "/dataviz/static/js/2.76a5d545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c33e00a6856bcdfb2c1",
+    "url": "/dataviz/static/js/main.45fb5afe.chunk.js"
+  },
+  {
+    "revision": "c970724c81956f9a8bb6",
+    "url": "/dataviz/static/js/runtime-main.03a3d94b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dataviz/static/media/logo.5d5d9eef.svg"
+  }
+]);
